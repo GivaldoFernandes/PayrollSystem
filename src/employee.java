4170aeb7c7b3e3302c1857deb3f81d358b79ae5c
GivaldoFernandes <givaldo.fernandes@live.com>
2014-09-27 20:53:47 -0300

    new files
    add employee.java, hourly.java and main.java

---
+++ src/employee.java
@@ -0,0 +1,10 @@
+
+public abstract class employee {
+	int id;
+	String name;
+	String andress;
+	double salary;
+	int paymentMethode; // Hourly, Salaried, Commissioned
+	boolean isSyndicate;
+	double syndicateTax;
+}
